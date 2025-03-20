2a43b60da29ed925e61d059f7958e3483491201e
newtmex <brightomex@gmail.com>
2025-03-20 18:20:55 +0100

    fix: correct start script to use 'ponder start' and simplify API routes

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,13 +1,9 @@
 import { ponder } from "ponder:registry";
-import { graphql } from "ponder";
 
 import yuzuSummary from "../../yuzuSummary";
 import landAvailInfo from "../../landAvailInfo";
 
-ponder.use("/graphql", graphql());
-ponder.use("/", graphql());
-
-ponder.get("land-avail-info", async (c) => {
+ponder.get("/", async (c) => {
     return c.json(await landAvailInfo(c.db));
 });
 
